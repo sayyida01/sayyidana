c584830669fffcdc54e9e3caf4d746d8340bbef1
sayyida01 <144520237+sayyida01@users.noreply.github.com>
2023-09-25 00:04:02 +0700

    Update F6.c

--- F6.c
+++ F6.c
@@ -4,5 +4,15 @@ int main(){
 	printf("===================================\n");
 	printf("=======DAFTAR MATA KULIAH==========\n");
 
-chat matakuliah[2][50] = {}
+chat matakuliah[2][50] = {"Algorithm", "Math Discrete"}; // Mendeklarasikan mata kuliah
+int SKS[2] = {4, 6}; // mendeklarasikan nilai sks
+char Grade[3] = {'A', 'A'}; // mendeklarasikan nilai
+printf("Kode Mata Kuliah\tMata Kuliah\t\tSKS\tGRADE\n");
+for (int i = 0; i < 2; i++) { 
+printf("%s\t\t\t%s\t\t%d\t%c\t\n", Kode_Mata_Kuliah[i],Mata_Kuliah[i], SKS[i], Grade[i]);
+}
+printf("\n========== TERIMAKASIH ============\n");
+
+return 0;
+}
 }
